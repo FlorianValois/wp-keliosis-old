255e3e6c7b55d3b157879dae84cd927392d73d21
Florian VALOIS <contact@florian-valois.com>
2018-09-12 10:14:58 +0200

    init

---
+++ wp-keliosis.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: WP-Keliosis
+Plugin URI: https://github.com/FlorianValois/wp-keliosis/
+Description:  A boilerplate plugin for WordPress with many options 
+Version: 0.1
+Author: Florian Valois
+Author URI: https://florian-valois.com
+Text Domain: wp-keliosis
+Domain Path: /languages
+GitHub Plugin URI: https://github.com/FlorianValois/wp-keliosis/
+*/
