5759ba1133d44b8109a0853a0fff45e04ce7a558
Florian VALOIS <contact@florian-valois.com>
2018-09-21 14:11:32 +0200

    change section in table format

--- templates/wpk_dashboard.php
+++ templates/wpk_dashboard.php
@@ -10,9 +10,12 @@ if ( !function_exists( 'wpk_dashboard' ) ) {
 	$data = get_option('wp_keliosis');
 
 	?>
-	<div id="<?= WPK_PREFIX.WPK_NAME ?>" class="wrap">
 
-		<section id="<?= WPK_PREFIX.'navigation' ?>">
+	<table id="<?= WPK_PREFIX.WPK_NAME ?>" class="wrap" cellspancing="0" cellpadding="0">
+		<tr>
+			
+		
+		<td id="<?= WPK_PREFIX.'navigation' ?>" valign="top">
 			<a id="<?= WPK_PREFIX.'logo' ?>" href=""><img src="https://via.placeholder.com/250x100" alt=""></a>
 			<nav>
 				<ul>
@@ -30,12 +33,11 @@ if ( !function_exists( 'wpk_dashboard' ) ) {
 							<span><?= __( 'Back to top', 'wp-keliosis' ); ?></span>
 						</a>
 					</li>
-
 				</ul>
 			</nav>
-		</section>
+		</td>
 
-		<section id="<?= WPK_PREFIX.'content' ?>">
+		<td id="<?= WPK_PREFIX.'content' ?>">
 			<form id="<?= WPK_PREFIX.'options' ?>" class="formAjax" method="post" action="">
 
 				<?php 
@@ -52,9 +54,11 @@ if ( !function_exists( 'wpk_dashboard' ) ) {
 			</div>
 
 			</form>
-		</section>
-
-	</div>
+		</td>
+		
+		
+		</tr>
+	</table>
 	<?php
 	}
 }
