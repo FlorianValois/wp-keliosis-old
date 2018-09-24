1de43d1d8ed63abb4c0f4df3b59c8e6d1509ce11
Florian VALOIS <contact@florian-valois.com>
2018-09-24 17:52:33 +0200

    remove input test dashboard

--- templates/includes/dashboard.inc.php
+++ templates/includes/dashboard.inc.php
@@ -12,8 +12,6 @@ if (!defined('ABSPATH')) {
 		<i class="fas fa-tachometer-alt"></i>
 		<h1><?= __( 'Dashboard', 'wp-keliosis' ); ?></h1>
 	</div>
-	
-	<input type="number" name="<?= WPK_BTT.'_HorizontalGap' ?>" value="<?php echo $data[WPK_BTT.'_HorizontalGap']; ?>" min="0">
 
 	
 </div>
